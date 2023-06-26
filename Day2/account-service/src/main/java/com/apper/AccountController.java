9d156585e0f003b1a75319e780e179f2ade51966
jmzarate09 <joshuazarate09@gmail.com>
2023-06-26 10:21:21 +0800

    Initial commit for Day2

---
+++ Day2/account-service/src/main/java/com/apper/AccountController.java
@@ -0,0 +1,15 @@
+package com.apper;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("account")
+public class AccountController {
+
+    @PostMapping
+    public... createAccount() {
+
+    }
+}
