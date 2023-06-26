0ea3b4ccef73a865616b17f35742a6c479188fda
jmzarate09 <joshuazarate09@gmail.com>
2023-06-26 15:05:59 +0800

    Updated IdGeneratorService

--- Day2/account-service/src/main/java/com/apper/IdGeneratorService.java
+++ Day2/account-service/src/main/java/com/apper/IdGeneratorService.java
@@ -1,5 +1,6 @@
 package com.apper;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -7,8 +8,13 @@ import java.util.UUID;
 @Service
 public class IdGeneratorService {
     public String generateRandomCharacters(int length) {
-        String randomString = UUID.randomUUID().toString().replaceAll("-", "");
-        return randomString.substring(0, length);
+
+        String generatedString = RandomStringUtils.randomAlphanumeric(length);
+        return generatedString;
+
+    //Alternative
+//        String randomString = UUID.randomUUID().toString().replaceAll("-", "");
+//        return randomString.substring(0, length);
     }
 
     public String getNextId() {
