d20726d08c44d43f779e21a4ecd6aa067b5d5280
jmzarate09 <joshuazarate09@gmail.com>
2023-07-04 14:08:46 +0800

    Updated Day 3 - Lab 1

--- Day3/e-store/src/main/java/com/apper/estore/ServiceExceptionHandler.java
+++ Day3/e-store/src/main/java/com/apper/estore/ServiceExceptionHandler.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 @ControllerAdvice
 public class ServiceExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseBody
     public ServiceError handleInvalidInputFields(MethodArgumentNotValidException ex) {
         return ex.getBindingResult().getAllErrors().stream()
@@ -22,7 +26,9 @@ public class ServiceExceptionHandler {
     }
 
     @ExceptionHandler(InvalidUserAgeException.class)
-    public ResponseEntity<String> handleInvalidUserAgeException(InvalidUserAgeException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<Object> handleInvalidUserAgeException(InvalidUserAgeException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
