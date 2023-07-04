5051db7d101568da5338fff4dab30e50d0b1dacd
jmzarate09 <joshuazarate09@gmail.com>
2023-07-04 11:46:53 +0800

    Added Scratch

---
+++ Day3/e-store-scratch/src/main/java/com/apper/estore/CreateUserRequest.java
@@ -0,0 +1,39 @@
+package com.apper.estore;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class CreateUserRequest {
+    @NotBlank(message = "Email is required") //Both NotNull and NotEmpty ---> to validate that a string is not null and ensures that the value is not empty
+    @Email(message = "Invalid Email Format") // used to validate that a string field or method parameter represents a valid email address. It ensures that the value follows the standard email format, including the presence of an "@" symbol
+    private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must have exactly 8 characters")
+    private String password;
+
+    @JsonProperty("first_name") //use this annotation to define a different name for a field or method in the JSON representation.
+    @NotBlank(message = "First Name is required")
+    private String firstName;
+    @JsonProperty("middle_name")
+    private String middleName;
+    @JsonProperty("last_name")
+    @NotBlank(message = "Last Name is required")
+    private String lastName;
+    @JsonProperty("birth_date")
+    @NotBlank(message = "BirthDay is required")
+    // @Pattern used to specify a regular expression pattern that a string field or method parameter must match. It allows you to define a custom pattern that the value must adhere to.
+        // the regular expression ^\d{4}-\d{2}-\d{2}$ ensures that the string matches the format "yyyy-dd-mm"
+        /*
+        * ' ^ ' denotes the start of the string. It ensures that the pattern matches from the beginning of the string.
+        * ' \d{4} ' matches exactly four digits. \d represents any digit from 0 to 9, and {4} specifies that it should occur exactly four times.
+        * ' $ ' denotes the end of the string. It ensures that the pattern matches until the end of the string.
+        *
+        * */
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Invalid date format. Please use yyyy-dd-mm.")
+    private String birthDate;
+}
---
+++ Day3/e-store-scratch/src/main/java/com/apper/estore/ServiceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.apper.estore;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice // indicates that it provides centralized exception handling for multiple controllers in the application.
+public class ServiceExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //annotation specifies that the HTTP response status code should be set to 400 (Bad Request) when this exception is encountered.
+    @ExceptionHandler(MethodArgumentNotValidException.class) // @ExceptionHandler(MethodArgumentNotValidException.class) annotation declares that this method should handle MethodArgumentNotValidException specifically.
+    @ResponseBody //The @ResponseBody annotation indicates that the return value of the method should be serialized directly into the response body.
+    // Within this class, there is a method handleInvalidInputFields that handles the MethodArgumentNotValidException specifically. This exception is typically thrown when the validation of input arguments fails in a controller method annotated with validation annotations such as @Valid.
+    public ServiceError handleInvalidInputFields(MethodArgumentNotValidException ex) {
+        //In the method implementation, it retrieves the binding result from the exception using ex.getBindingResult(). The binding result contains information about the validation errors that occurred.
+            // findFirst() to retrieve the first error, and if it exists, it creates a new ServiceError object using the error's default message. If there are no errors, it creates a generic ServiceError object with the message "Unknown invalid argument encountered".
+        return ex.getBindingResult().getAllErrors().stream()
+                .findFirst()
+                .map(objectError -> new ServiceError(objectError.getDefaultMessage()))
+                .orElse(new ServiceError("Unknown invalid argument encountered"));
+    }
+}
