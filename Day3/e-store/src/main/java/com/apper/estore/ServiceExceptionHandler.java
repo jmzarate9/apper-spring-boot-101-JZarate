a3f372681385b2448dc84a79a84a232c464d93de
jmzarate09 <joshuazarate09@gmail.com>
2023-07-04 14:44:39 +0800

    Latest Updated Day 3 - Lab 1

--- Day3/e-store/src/main/java/com/apper/estore/ServiceExceptionHandler.java
+++ Day3/e-store/src/main/java/com/apper/estore/ServiceExceptionHandler.java
@@ -25,10 +25,10 @@ public class ServiceExceptionHandler {
                 .orElse(new ServiceError("Unknown invalid argument encountered"));
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(InvalidUserAgeException.class)
-    public ResponseEntity<Object> handleInvalidUserAgeException(InvalidUserAgeException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    @ResponseBody
+    public ServiceError handleInvalidUserAgeException(InvalidUserAgeException ex) {
+        return new ServiceError(ex.getMessage());
     }
 }
